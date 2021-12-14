a902c0fa4e7c27479253fa528a61583a2f2ca083
Alejandra Diaz Rojas <ale.diaz.5@hotmail.com>
2021-12-14 18:23:30 -0500

    first mainfeatures draft

--- src/pages/MainFeatures.jsx
+++ src/pages/MainFeatures.jsx
@@ -1,2 +1,30 @@
-//This is the space for the main features
-// :)
+import React from 'react'
+
+import '../styles/pages/MainFeatures.scss'
+
+const MainFeatures = () => {
+return (
+    <div className="mainfeatures">
+
+    Main features
+        ALejandragrijegoiejrog
+        <section className='cards-container'>
+            <div class='card'><h1></h1>Networks
+            <p>
+                Sync your profile with available social media
+            </p></div>
+            <div class='card'>Statistics
+            <p> Chekc your statistics</p></div>
+            <div class='card'>Tracker
+            <p>MAke a tracking of your job applications</p></div>
+
+
+
+
+        </section>
+
+    </div>
+)
+}
+
+export default MainFeatures
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -5,6 +5,7 @@ import Login from '../pages/Login'
 import NotFound from '../containers/NotFound'
 import Home from '../pages/Home'
 import LoginWith from '../components/LoginWith'
+import MainFeatures from '../pages/MainFeatures'
 import '../styles/global.css'
 import Layout from '../containers/Layout'
 
@@ -17,6 +18,7 @@ const App = () => {
           <Route exact path="/home" component={Home}/>
           <Route exact path="/login" component={Login}/>
           <Route exact path="/login_with" component={LoginWith}/>
+          <Route exact path="/main_features" component={MainFeatures}/>
         </Layout>
       </Switch>
     </BrowserRouter>
