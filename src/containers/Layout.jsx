485996086094001aebd3e90a32bbf50c7ede01d5
Angel <angel.aven.c@gmail.com>
2021-12-14 18:02:27 -0600

    Logo header

--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -9,7 +9,7 @@ const Layout = ({ children }) => {
     <div className="Layout">
       <HeaderContent>
         <Container>
-          <img src="../public/assets/img/logo.svg" alt="logo" />
+          <img src="https://raw.githubusercontent.com/AngAven/mf-social-media-test/1f04b5afb08763732b900c02a02fbda896045a52/public/assets/img/logo.svg" alt="logo" />
           {/* <Logotype width={140} />   */}
 
         </Container>
