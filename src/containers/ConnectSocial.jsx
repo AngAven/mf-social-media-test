db7a5b4ba728b4510565aafb13f73a92c737a5fa
Angel <angel.aven.c@gmail.com>
2021-12-15 22:01:01 -0600

    Icon Logos From coonect social has changed

--- src/containers/ConnectSocial.jsx
+++ src/containers/ConnectSocial.jsx
@@ -8,9 +8,9 @@ const ConnectSocial = () => {
     <>
       <h1 className={'title'}>Connect Social Network</h1>
         <Container >
-          <CardConnectSocial social_name={"Twitter"} social_image={'https://raw.githubusercontent.com/AngAven/mf-social-media-test/1ef7dc070e3524db2f9897fa875a6e319768309e/public/assets/img/tw.svg'}/>
-          <CardConnectSocial social_name={"Facebook"} social_image={'https://raw.githubusercontent.com/AngAven/mf-social-media-test/1ef7dc070e3524db2f9897fa875a6e319768309e/public/assets/img/fb.svg'}/>
-          <CardConnectSocial social_name={"LinkedIn"} social_image={'https://raw.githubusercontent.com/AngAven/mf-social-media-test/1ef7dc070e3524db2f9897fa875a6e319768309e/public/assets/img/li.svg'}/>
+          <CardConnectSocial social_name={"Twitter"} social_image={'https://raw.githubusercontent.com/AngAven/mf-social-media-test/bbc084e1fb1cb40b2ec3033efbf34910a47b74af/public/icons/tw-icon.svg'}/>
+          <CardConnectSocial social_name={"Facebook"} social_image={'https://raw.githubusercontent.com/AngAven/mf-social-media-test/bbc084e1fb1cb40b2ec3033efbf34910a47b74af/public/icons/fb-icon.svg'}/>
+          <CardConnectSocial social_name={"LinkedIn"} social_image={'https://raw.githubusercontent.com/AngAven/mf-social-media-test/bbc084e1fb1cb40b2ec3033efbf34910a47b74af/public/icons/li-icon.svg'}/>
         </Container>
     </>
   )
