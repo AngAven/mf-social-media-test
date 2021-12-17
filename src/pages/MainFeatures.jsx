e42d7c0ca470378d7be36225912dd7dc68c97dff
Angela Uribe <ing.angelauribe@outlook.com>
2021-12-17 17:06:34 -0500

    MainFeatures

--- src/pages/MainFeatures.jsx
+++ src/pages/MainFeatures.jsx
@@ -16,22 +16,22 @@ const MainFeatures = () => {
 
             <div className="mainfeatures">
                 <section className='cards-container'>
-                    <div class='card'><h1>Networks</h1>
+                    <div className='card'><h1>Networks</h1>
                         <img id="imgnetwroks" src='/img/networks.jpg'></img>
                         <p>
                             Sync your profile with available social media
                         </p>
                         <Link to="/connect-social">
-                            <button claasName="gradient">
+                            <button className="gradient">
                                 GO
                             </button>
                         </Link></div>
 
-                    <div class='card'><h1>Statistics</h1>
+                    <div className='card'><h1>Statistics</h1>
                         <img id="imgstats" src='/img/stats.jpg'></img>
                         <p> Check your statistics</p>
                         <button>GO </button></div>
-                    <div class='card'><h1>Tracker</h1>
+                    <div className='card'><h1>Tracker</h1>
                         <img id="imgtracker" src='/img/tracker.jpg'></img>
                         <p>Make a tracking of your job applications</p>
                         <button> GO </button></div>
