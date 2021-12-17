3bc416aa345c2591acb95988ceb2eaca29ec9cbd
Angel <angel.aven.c@gmail.com>
2021-12-16 18:48:40 -0600

    Testing CI/CD

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Home = () => {
   return (
     <div>
-      Mi home
+      <Link to="/login">login</Link>
     </div>
   )
 }
