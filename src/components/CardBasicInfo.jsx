ce21500e101b95d8ac2ce56bc169c53922f3208d
Angela Uribe <ing.angelauribe@outlook.com>
2022-01-25 22:54:00 -0500

    CardComponents

--- src/components/CardBasicInfo.jsx
+++ src/components/CardBasicInfo.jsx
@@ -1,17 +1,18 @@
-import React, {useContext} from 'react'
+import React, { useContext } from 'react'
 import AppContext from '@context/AppContext'
 import Card from '@material-ui/core/Card'
+import { CardMedia } from '@mui/material'
 import CardContent from '@material-ui/core/CardContent'
 import Typography from '@mui/material/Typography'
-import {IconButton, makeStyles} from '@material-ui/core'
+import { IconButton, makeStyles } from '@material-ui/core'
 import Avatar from '@material-ui/core/Avatar'
-import {yellow, green, pink, blue} from '@material-ui/core/colors'
+import { yellow, green, pink, blue } from '@material-ui/core/colors'
 import EmailIcon from '@mui/icons-material/Email'
 import CakeIcon from '@mui/icons-material/Cake'
 import TwitterIcon from '@mui/icons-material/Twitter'
 import LanguageIcon from '@mui/icons-material/Language'
 import ListItem from '@mui/material/ListItem'
-import {List} from '@mui/material'
+import { List } from '@mui/material'
 
 
 const useStyles = makeStyles({
@@ -25,61 +26,44 @@ const useStyles = makeStyles({
   }
 })
 
-export default function CardBasicInfo(){
+export default function CardBasicInfo() {
   const classes = useStyles(facebook)
-  const {state} = useContext(AppContext)
-  const {facebook, linkedin, twitter} = state
+  const { state } = useContext(AppContext)
+  const { facebook, linkedin, twitter } = state
   const IconItems = [
     {
-      text: 'Email',
-      icon: <EmailIcon color="primary" style={{fontSize: 30}}/>,
+      text: <>{facebook.email}</>,
+      icon: <EmailIcon color="primary" style={{ fontSize: 30 }} />,
     },
     {
-      text: 'Birthday',
-      icon: <CakeIcon color="primary" style={{fontSize: 30}}/>,
+      text: <>{facebook.birthday}</>,
+      icon: <CakeIcon color="primary" style={{ fontSize: 30 }} />,
     },
     {
-      text: 'Country',
-      icon: <TwitterIcon color="primary" style={{fontSize: 30}}/>,
+      text: <>{facebook.twitter}</>,
+      icon: <TwitterIcon color="primary" style={{ fontSize: 30 }} />,
     },
     {
-      text: 'Language',
-      icon: <LanguageIcon color="primary" style={{fontSize: 30}}/>,
+      text: <>{facebook.language}</>,
+      icon: <LanguageIcon color="primary" style={{ fontSize: 30 }} />,
     }
   ]
 
   return (
     <div>
+    
       <Card elevation={1}>
         <CardContent>
           <Typography variant="h4">
             {facebook.name}
           </Typography>
 
-          <ListItem>
-            <IconButton><EmailIcon color="primary" fontSize="large"/> </IconButton>
-            <Typography variant="h5">{facebook.email} </Typography>
-          </ListItem>
-          <ListItem>
-            <IconButton><CakeIcon color="primary" fontSize="large"/> </IconButton>
-            <Typography variant="h5">{facebook.birthday} </Typography>
-          </ListItem>
-          <ListItem>
-            <IconButton><TwitterIcon color="primary" fontSize="large"/> </IconButton>
-            <Typography variant="h5">{facebook.twitter} </Typography>
-          </ListItem>
-          <ListItem>
-            <IconButton><LanguageIcon color="primary" fontSize="large"/> </IconButton>
-            <Typography variant="h5">{facebook.language} </Typography>
-          </ListItem>
-
-
-          {  /*  IconItems.map(item => (
-                            <ListItem key={item.text}>
-                                <IconButton >{item.icon} </IconButton>
-                                <Typography variant='h5' >{item.text} </Typography>
-                            </ListItem>
-                        )) */
+          {IconItems.map((item, i) => (
+            <ListItem key={i}>
+              <IconButton >{item.icon} </IconButton>
+              <Typography variant='h5' >{item.text} </Typography>
+            </ListItem>
+          ))
           }
         </CardContent>
       </Card>
--- src/pages/IntegrationProfile.jsx
+++ src/pages/IntegrationProfile.jsx
@@ -3,14 +3,19 @@ import AppContext from '@context/AppContext'
 import {Card, CardHeader, Container} from '@mui/material'
 import {IconButton, makeStyles} from '@material-ui/core'
 import {CardContent, Grid} from '@mui/material'
-import CardProfilePicture from '../components/CardProfilePicture'
-import CardBasicInfo from '../components/CardBasicInfo'
+import CardProfilePicture from '@components/CardProfilePicture'
+import CardBasicInfo from '@components/CardBasicInfo'
 import SchoolIcon from '@mui/icons-material/School'
 import WorkIcon from '@mui/icons-material/Work'
 import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt'
 import InterestsIcon from '@mui/icons-material/Interests'
 import Typography from '@mui/material/Typography'
-import CardIntegration from '../components/CardIntegration'
+import CardIntegration from '@components/CardIntegration'
+import CardEducation from '@components/CardEducation'
+import CardWork from '@components/CardWork'
+import CardFollows from '@components/CardFollows'
+import CardInterest from '../components/CardInterest'
+
 
 const useStyles = makeStyles({
   profilecard: {
@@ -28,18 +33,22 @@ const IntegrationProfile = () => {
     {
       text: 'Education',
       icon: <SchoolIcon color="primary" style={{fontSize: 30}}/>,
+      comp: <CardEducation/>
     },
     {
       text: 'Work',
       icon: <WorkIcon color="primary" style={{fontSize: 30}}/>,
+      comp: <CardWork/>
     },
     {
       text: 'Follows',
       icon: <ThumbUpAltIcon color="primary" style={{fontSize: 30}}/>,
+      comp: <CardFollows/>
     },
     {
       text: 'Interest',
       icon: <InterestsIcon color="primary" style={{fontSize: 30}}/>,
+      comp: <CardInterest/>
     }
   ]
 
@@ -47,7 +56,7 @@ const IntegrationProfile = () => {
     <Container>
       <Grid container spacing={4}>
 
-        <Grid item xs={12} md={6} lg={6}>
+        <Grid item xs={12} md={6} lg={6} >
           <CardProfilePicture/>
         </Grid>
 
