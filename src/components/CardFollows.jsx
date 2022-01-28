1270d52eda977884f667718a1a1cb1f3d85173fe
Alejandra Diaz <ale.diaz.5@hotmail.com>
2022-01-28 15:27:33 -0500

    Follow Card, Interests Card Update

--- src/components/CardFollows.jsx
+++ src/components/CardFollows.jsx
@@ -1,6 +1,11 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
+import { Box } from '@mui/system';
+import { Paper } from '@material-ui/core'
+import { Grid } from '@material-ui/core'
+import { Card } from '@material-ui/core'
+import { ListItemButton } from '@mui/material'
 import { ListItemIcon } from '@material-ui/core'
 import { ListItemText } from '@material-ui/core'
 import FacebookIcon from '@mui/icons-material/Facebook';
@@ -8,64 +13,100 @@ import TwitterIcon from '@mui/icons-material/Twitter';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import { Typography } from "@mui/material";
 import AppContext from '@context/AppContext'
+import { styled } from '@mui/material/styles';
 
 export default function CardFollows() {
 
     const { state } = useContext(AppContext)
     const { facebook, linkedin, twitter } = state
+
+    const [selectedIndex,setSelectedIndex] = useState(1)
+
     const followItems = [
         {
+          id: "3",
           socialmedia: "facebook",
           following: <>{facebook.follow_people}</>,
           followers: <>{facebook.followers}</>,
-          icon:<FacebookIcon color="primary" style={{ fontSize: 25 }}/>
+          icon:<FacebookIcon color="primary" style={{ fontSize: 35 }}/>
         },
         {
+            id: "2",
             socialmedia: "twitter",
             following: <>{twitter.follow_people}</>,
             followers: <>{twitter.followers}</>,
-            icon:<TwitterIcon color="primary" style={{ fontSize: 25 }}/>
+            icon:<TwitterIcon color="primary" style={{ fontSize: 35 }}/>
           }
         ,
         {
-            socialmedia: "linkedIn",
+            id: "1",
+            socialmedia: "linkedin",
             following: <>{linkedin.follow_people}</>,
             followers: <>{linkedin.followers}</>,
-            icon:<LinkedInIcon color="primary" style={{ fontSize: 25 }} />
+            icon:<LinkedInIcon color="primary" style={{ fontSize: 35 }} />
           }
       ]
 
-    const flexContainer = {
+      const handleListItemClick = (event, id) => {
+        setSelectedIndex(id);
+      };
+
+    const rules = {
         display: 'flex',
         flexDirection: 'row',
         padding: 0,
+        bottom:0,
+        height:140,
+        justifyContent: 'center',
+        alignItems: 'center',
+
         }
 
     const items ={
         flexDirection: 'column',
-        alignItems: 'center'
+        alignItems: 'center',
+
     }
 
     return (
         <div>
-        <List style={flexContainer}>
-            {followItems.map(item => (
-                <ListItem style={items}
-                key={item.socialmedia}>
-                    <ListItemIcon>
+
+        <Grid container spacing={8}>
+            <Grid item xs={3}>
+                <List xs={2}>
+                    {followItems.map(item => (
+                        <ListItemButton
+                        key={item.id}
+                        style={items}
+                        md={5}
+                        selected={selectedIndex == `${item.id}`}
+                        onClick={(event) =>handleListItemClick(event,`${item.id}`)}>
                         {item.icon}
-                    </ListItemIcon>
-                    <ListItemText
-                    primary={<Typography variant="h6">Following</Typography>}
-                    secondary={item.following}>
-                    </ListItemText>
-                    <ListItemText
-                    primary={<Typography variant="h6">Followers</Typography>}
-                    secondary={item.followers}>
-                    </ListItemText>
-                </ListItem>
-            ))}
-        </List>
+                        </ListItemButton>
+                    ))}
+                </List>
+            </Grid>
+
+            <Grid item xs={4} >
+                <Paper style={rules} >
+                    <Box >
+                    <Typography sx={{padding:1, textAlign:'center'}} variant="h3">
+                    {followItems[0].followers}</Typography>
+                    <Typography sx={{padding:1, textAlign:'center'}} variant="h5">Followers</Typography>
+                    </Box>
+                </Paper>
+             </Grid>
+             <Grid item xs={4} >
+                <Paper style={rules} >
+                    <Box >
+                    <Typography sx={{padding:1, textAlign:'center'}} variant="h3">
+                        {followItems[0].following}</Typography>
+                    <Typography sx={{padding:1, textAlign:'center'}} variant="h5">Following</Typography>
+                    </Box>
+                </Paper>
+             </Grid>
+
+        </Grid>
 
         </div>
     )
--- src/components/CardInterest.jsx
+++ src/components/CardInterest.jsx
@@ -1,9 +1,57 @@
-import React from "react";
+import React, { useContext, useState } from 'react'
+import { Divider, Stack } from '@mui/material'
+import { Chip } from '@mui/material'
+import { Grid } from '@material-ui/core'
+import AppContext from '@context/AppContext'
+import { Box } from '@mui/system';
 
 export default function CardInterest() {
+
+    const { state } = useContext(AppContext)
+    const { facebook, linkedin, twitter } = state
+
     return (
         <div>
+            <Divider style={{width:'100%',padding:1, fontSize:'1rem'}}>FACEBOOK</Divider>
+            <Stack
+                direction="row"
+                justifyContent="space-around"
+                alignItems="center"
+                spacing={1}
 
+                flexWrap='wrap'
+            >
+                {facebook.groups && facebook.groups.map(group=>(
+                    <Chip label={group.name} >
+                    </Chip>
+                ))}
+            </Stack>
+            <Divider style={{width:'100%',padding:1, fontSize:'1rem'}}>TWITTER</Divider>
+            <Stack
+                direction="row"
+                justifyContent="space-around"
+                alignItems="center"
+                spacing={1}
+                flexWrap='wrap'
+            >
+                {twitter.groups && twitter.groups.map(group=>(
+                    <Chip label={group.name}>
+                    </Chip>
+                ))}
+            </Stack>
+            <Divider style={{width:'100%',padding:1, fontSize:'1rem'}}>LINKEDIN</Divider>
+            <Stack
+                direction="row"
+                justifyContent="space-around"
+                alignItems="center"
+                spacing={1}
+                flexWrap='wrap'
+            >
+                {linkedin.groups && linkedin.groups.map(group=>(
+                    <Chip label={group.name}>
+                    </Chip>
+                ))}
+            </Stack>
         </div>
     )
 }
