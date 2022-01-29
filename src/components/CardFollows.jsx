acb2f87e5f8ac781afc9c4266839eded68890dd7
Alejandra Diaz <ale.diaz.5@hotmail.com>
2022-01-28 22:24:24 -0500

    FollowCard Updated

--- src/components/CardFollows.jsx
+++ src/components/CardFollows.jsx
@@ -1,6 +1,13 @@
 import React, { useContext, useState } from 'react'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
+import { Divider } from '@mui/material'
 import { Box } from '@mui/system';
 import { Paper } from '@material-ui/core'
 import { Grid } from '@material-ui/core'
@@ -20,94 +27,57 @@ export default function CardFollows() {
     const { state } = useContext(AppContext)
     const { facebook, linkedin, twitter } = state
 
-    const [selectedIndex,setSelectedIndex] = useState(1)
-
     const followItems = [
         {
-          id: "3",
+          id: "1",
           socialmedia: "facebook",
           following: <>{facebook.follow_people}</>,
           followers: <>{facebook.followers}</>,
-          icon:<FacebookIcon color="primary" style={{ fontSize: 35 }}/>
+          icon:<FacebookIcon color="primary" style={{ fontSize: 25 }}/>
         },
         {
             id: "2",
             socialmedia: "twitter",
             following: <>{twitter.follow_people}</>,
             followers: <>{twitter.followers}</>,
-            icon:<TwitterIcon color="primary" style={{ fontSize: 35 }}/>
+            icon:<TwitterIcon color="primary" style={{ fontSize: 25 }}/>
           }
         ,
         {
-            id: "1",
+            id: "3",
             socialmedia: "linkedin",
             following: <>{linkedin.follow_people}</>,
             followers: <>{linkedin.followers}</>,
-            icon:<LinkedInIcon color="primary" style={{ fontSize: 35 }} />
+            icon:<LinkedInIcon color="primary" style={{ fontSize: 25 }} />
           }
       ]
-
-      const handleListItemClick = (event, id) => {
-        setSelectedIndex(id);
-      };
-
-    const rules = {
-        display: 'flex',
-        flexDirection: 'row',
-        padding: 0,
-        bottom:0,
-        height:140,
-        justifyContent: 'center',
-        alignItems: 'center',
-
-        }
-
-    const items ={
-        flexDirection: 'column',
-        alignItems: 'center',
-
-    }
+;
 
     return (
         <div>
-
-        <Grid container spacing={8}>
-            <Grid item xs={3}>
-                <List xs={2}>
-                    {followItems.map(item => (
-                        <ListItemButton
-                        key={item.id}
-                        style={items}
-                        md={5}
-                        selected={selectedIndex == `${item.id}`}
-                        onClick={(event) =>handleListItemClick(event,`${item.id}`)}>
-                        {item.icon}
-                        </ListItemButton>
-                    ))}
-                </List>
-            </Grid>
-
-            <Grid item xs={4} >
-                <Paper style={rules} >
-                    <Box >
-                    <Typography sx={{padding:1, textAlign:'center'}} variant="h3">
-                    {followItems[0].followers}</Typography>
-                    <Typography sx={{padding:1, textAlign:'center'}} variant="h5">Followers</Typography>
-                    </Box>
-                </Paper>
-             </Grid>
-             <Grid item xs={4} >
-                <Paper style={rules} >
-                    <Box >
-                    <Typography sx={{padding:1, textAlign:'center'}} variant="h3">
-                        {followItems[0].following}</Typography>
-                    <Typography sx={{padding:1, textAlign:'center'}} variant="h5">Following</Typography>
-                    </Box>
-                </Paper>
-             </Grid>
-
-        </Grid>
-
+            <TableContainer component={Paper}>
+                <Table aria-label="simple table">
+                    <TableHead>
+                        <TableRow sx={{"& th": {fontSize:"1.5rem"}, backgroundColor:"#f6f6f6"}}>
+                            <TableCell align="center">Social Media</TableCell>
+                            <TableCell align="center">Following</TableCell>
+                            <TableCell align="center">Followers</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {followItems.map((item)=> (
+                            <TableRow
+                            key={item.id}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0}}}
+                            >
+                            <TableCell align="center">{item.icon}</TableCell>
+                            <TableCell align="center" sx={{fontSize:"1.25rem"}}>{item.followers}</TableCell>
+                            <TableCell align="center" sx={{fontSize:"1.25rem"}}>{item.following}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+               </Table>
+            </TableContainer>
         </div>
     )
 }
