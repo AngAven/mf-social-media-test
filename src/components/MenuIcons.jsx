fcf7f95e74df2adde99400bff11bb44a27de4684
Alejandra Diaz <ale.diaz.5@hotmail.com>
2022-02-04 00:03:55 -0500

    Added Button for Analytics and changed icons in MenuIcons

--- src/components/MenuIcons.jsx
+++ src/components/MenuIcons.jsx
@@ -1,13 +1,16 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import IntegrationIcon from '@mui/icons-material/Web';
-import StatisticIcon from '@mui/icons-material/BarChart';
 import TrackerIcon from '@mui/icons-material/AutoGraph';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-
+import DynamicFeedIcon from '@mui/icons-material/DynamicFeed';
+import ExtensionOutlinedIcon from '@mui/icons-material/ExtensionOutlined';
+import SavedSearchIcon from '@mui/icons-material/SavedSearch';
+import { Typography } from '@mui/material';
 
 export default function SimpleBottomNavigation() {
+
+
   const [value, setValue] = React.useState(0);
 
   return (
@@ -15,13 +18,15 @@ export default function SimpleBottomNavigation() {
       <BottomNavigation
         showLabels
         value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
+        onChange={(event, newValue,theme) => {
+          setValue(newValue,theme);
+
         }}
       >
-        <BottomNavigationAction label="Integration" icon={<IntegrationIcon  />} />
-        <BottomNavigationAction label="Statistic" icon={<StatisticIcon  />} />
-        <BottomNavigationAction label="Tracker" icon={<TrackerIcon  />} />
+      <BottomNavigationAction label={<Typography sx={{fontSize:14}}>Integrations</Typography>} icon={<DynamicFeedIcon sx={{fontSize:23}} />}/>
+      <BottomNavigationAction label={<Typography sx={{fontSize:14}}>Statistic</Typography>} icon={<ExtensionOutlinedIcon  sx={{fontSize:23}}/>}/>
+      <BottomNavigationAction label={<Typography sx={{fontSize:14}}>Tracker</Typography>} icon={<SavedSearchIcon  sx={{fontSize:23}}/>}/>
+      <BottomNavigationAction label={<Typography sx={{fontSize:14}}>Analytics</Typography>} icon={<TrackerIcon sx={{fontSize:23}} />}/>
       </BottomNavigation>
     </Box>
   );
